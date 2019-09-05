5914c5f492500344482ceb38708dc7590f2c7315
Collins <Brett.Collins@navistar.com>
2019-09-05 17:19:44 -0500

    intital commit

---
+++ yahoo_daily_fantasy.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 13:00:42 2019
+
+@author: u00bec7
+"""
+
+## Packages
+
+
+import pandas as pd
+import os
+import numpy as np
+
+#%% FUNCTIONS
+
+
+
+#%% LOAD DATA
+
+
+os.chdir('C:\\Users\\u00bec7\\Desktop\\personal\\ff\\data')
+
+
+data = pd.read_csv('Yahoo_DF_player_export.csv')
