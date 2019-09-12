5bce05a39cd79eddbddfff28e96377a1951a46b9
Collins <Brett.Collins@navistar.com>
2019-09-12 13:04:04 -0500

    start LP modeling

--- yahoo_daily_fantasy.py
+++ yahoo_daily_fantasy.py
@@ -11,6 +11,7 @@ Created on Thu Sep  5 13:00:42 2019
 import pandas as pd
 import os
 import numpy as np
+import pulp
 
 #%% FUNCTIONS
 
@@ -21,5 +22,40 @@ import numpy as np
 
 os.chdir('C:\\Users\\u00bec7\\Desktop\\personal\\ff\\data')
 
+data = pd.read_csv('Yahoo_DF_player_export_w2.csv')
 
-data = pd.read_csv('Yahoo_DF_player_export.csv')
+
+
+for position in ['QB', 'TE', 'RB', 'DEF', 'WR']:
+    data.loc[:,position] = (data['Position'] == position) * 1
+    
+
+data.loc[:,'FLEX'] = data[['TE', 'RB', 'WR']].sum(axis = 1)
+    
+positionLimit = {'QB':1
+                 , 'TE':1
+                 , 'RB':2
+                 , 'DEF':1
+                 , 'WR':3
+                 , 'FLEX':1
+                 }
+
+statusExclude = ['IR', 'SUSP', 'O', 'Q']
+
+
+
+dataInput = data[data['Injury Status'].map(lambda s: s not in statusExclude)]
+
+
+dataInputDict = dataInput.set_index('Id').to_dict('index')
+
+
+# Setup LP Problem
+
+prob = pulp.LpProblem('The Best Team', pulp.LpMaximize)
+
+# Define player Variables
+playerVars = pulp.LpVariable.dicts('ID', list(dataInput['Id']), cat = 'Binary')
+
+
+
